6a57e8cc5c6fde3eb635936ba795ffd276dcad1e
VictoriaNadia <victoria.gonzalez@arbusta.net>
2022-08-24 12:16:46 -0300

    db changes + examples

--- back/database/db.js
+++ back/database/db.js
@@ -5,9 +5,10 @@ const dotenv = require('dotenv')
 dotenv.config({path: './env/.env'}) 
 //Declare our db
 const db = new Sequelize(process.env.DB_DATABASE,process.env.DB_USER,process.env.DB_PASS,{
-    host : process.env.DB_HOST, 
-    dialect : 'mysql', 
-    port : 3306 
+
+    host : process.env.DB_HOST,
+    dialect : 'mysql',
+    port : process.env.DB_PORT
 });
 //Export our db
 module.exports = db;
