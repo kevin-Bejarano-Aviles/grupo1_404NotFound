9b45e5da33868f6b8824e4402b15a0ef010863e1
Guada Garcilazo <guadalupe.garcilazo@arbusta.net>
2022-08-24 16:34:16 -0300

    cambios nuevos en componentes

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import WelcomePage from './pages/WelcomePage';
 
 function App() {
   return (
     <div className="App">
+      <WelcomePage/>
     </div>
   );
 }
---
+++ front/src/pages/WelcomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+
+
+const WelcomePage = () => {
+  return (
+    <div className="bg-pink-200">
+        <button className="bg-red-400">Hoal</button>
+
+    </div>
+  )
+};
+ export default WelcomePage;
