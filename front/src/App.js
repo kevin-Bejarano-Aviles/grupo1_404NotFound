23f8d32f2376a74c0e83bae91692bbf399fc5871
lautaroncoria <lautaro.coria@arbusta.net>
2022-08-24 16:56:39 -0300

    Rutas creadas en App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import WelcomePage from './pages/WelcomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
     <div className="App">
-      <WelcomePage/>
+      <Routes>
+        <Route path={'/'} element={<WelcomePage />} />
+        <Route path={'/login'} element={<LoginPage />} />
+        <Route path={'/sign-up'} element={<SignUpPage />} />
+        <Route path={'/home'} element={<HomePage />} />
+        <Route path={'/profile'} element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 }
