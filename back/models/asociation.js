3ea6294b42265b235f54a958b2f86835ae81cc86
camiAguirre4rb1 <camila.aguirre@arbusta.net>
2022-08-24 14:42:10 -0300

    ORM-sequelize-mysql-relations

--- back/models/asociation.js
+++ back/models/asociation.js
@@ -2,10 +2,16 @@
 const userModel = require('./usersModel');
 const commentModel = require('./commentsModel');
 const postModel = require('./postsModel');
-//
+//Define relations
 userModel.hasMany(postModel,{foreignKey:'usersId'});
 postModel.belongsTo(userModel,{foreignKey:'usersId'});
 postModel.hasmany(commentModel,{foreignKey:'postId'});
 commentModel.belongsTo(postModel,{foreignKey:'postId'});
 commentModel.belongsTo(userModel,{foreignKey:'usersId'});
 userModel.hasMany(commentModel,{foreignKey:'usersId'});
+//export models with their relations
+module.exports = {
+    commentModel,
+    postModel,
+    userModel
+};
