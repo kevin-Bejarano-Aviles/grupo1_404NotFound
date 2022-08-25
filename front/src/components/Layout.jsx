383b8a761f673bd8d5824f9f684e31d065899afa
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-08-25 15:29:27 -0300

    nav e imagenes

--- front/src/components/Layout.jsx
+++ front/src/components/Layout.jsx
@@ -1,17 +1,19 @@
-import * as React from 'react'
+import * as React from 'react';
+import plus from '../img/plus_icon_brown.svg';
+import home from '../img/home_icon_brown.svg';
+import user from '../img/user_icon_brown.svg';
 import { Link , Outlet } from 'react-router-dom';
 function Layout(){
     return(
-    <div>
-        <nav>
-            <Link to={'home'}>Boton 1</Link>
-            <Link to={'profile'}>Boton 2</Link>
+    <>
+        <nav className='fixed h-20 w-full flex justify-around items-center bg-pastelpink  bottom-0 right-0 left-0'>
+            <Link to={'/home'}><img className='w-15' src={home} alt='user'/></Link>
+            <div className=''>
+                <img className='w-20 h-20 ' src={plus} alt='user'/>
+            </div>
+            <Link to={'/profile'}><img className='w-17 h-17' src={user} alt='home'/></Link>
         </nav>
-        <section>
-            <Outlet/>
-        </section>
-    </div>
+    </>
     )
 }
-
 export default Layout;
