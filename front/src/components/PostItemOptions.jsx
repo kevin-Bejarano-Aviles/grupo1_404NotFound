d75de574c3ec5c57dfdf8198cd4594397e0bd2e6
lautaroncoria <lautaro.coria@arbusta.net>
2022-08-26 18:13:46 -0300

    Se creó componente PostItemOptions

---
+++ front/src/components/PostItemOptions.jsx
@@ -0,0 +1,34 @@
+import editIcon from '../img/icon-edit.svg';
+import trashIcon from '../img/icon-trash.svg';
+
+function PostItemOptions({ classList, disabledState, setDisabledState }) {
+
+  const editPost = () => {
+    setDisabledState();
+    document.getElementById('postbody').focus();
+  };
+
+  return (
+    <div className={`${classList} absolute right-16`}>
+      <ul className='flex gap-1'>
+        <li>
+          <img
+            className={`${disabledState ? '' : 'bg-pastelgray'} p-2 rounded-full hover:bg-pastelgray`}
+            src={editIcon}
+            alt='Editar Publicación'
+            onClick={() => editPost()}
+          />
+        </li>
+        <li>
+          <img
+            className='p-2 rounded-full hover:bg-pastelgray'
+            src={trashIcon}
+            alt='Eliminar Publicación'
+            />
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default PostItemOptions;
