4eb51e28a42328cbcdae0fb1531bf47943906df7
lautaroncoria <lautaro.coria@arbusta.net>
2022-08-26 18:20:07 -0300

    Ahora se puede editar un Post

--- front/src/components/PostItem.jsx
+++ front/src/components/PostItem.jsx
@@ -6,6 +6,7 @@ import likeIcon from '../img/icon-like.svg';
 import commentIcon from '../img/icon-comment.svg';
 import messageIcon from '../img/icon-message.svg';
 import iconThreeDots from '../img/icon-three-dots.svg';
+import PostItemOptions from './PostItemOptions';
 
 function PostItem({ item }) {
 
@@ -26,10 +27,12 @@ function PostItem({ item }) {
     }
   ];
 
-  const [state, setState] = useState(false);
+  const [commentState, setCommentState] = useState(false);
+  const [menuState, setMenuState] = useState(false);
+  const [disabledState, setDisabledState] = useState(true);
 
-  const toggleState = () => {
-    state ? setState(false) : setState(true);
+  const changeCommentState = () => {
+    commentState ? setCommentState(false) : setCommentState(true);
   };
 
   const sendComment = e => {
@@ -37,33 +40,79 @@ function PostItem({ item }) {
     console.log(e.target.previousElementSibling.value);
   };
 
+  const showOptionsWindow = () => {
+    menuState ? setMenuState(false) : setMenuState(true);
+  };
+
   return (
     <article className='bg-white rounded-2xl shadow-xl overflow-hidden'>
-      <header className='flex justify-between px-6 pt-4'>
+      <header className='relative flex justify-between items-center px-6 pt-4'>
         <div className='flex items-center gap-4'>
-          <img className='w-10 rounded-full' src={avatar} alt="Avatar" />
+          <img className='w-10 rounded-full' src={avatar} alt='Avatar' />
           <span className='font-semibold'>{item.name} {item.surname}</span>
         </div>
-        <img src={iconThreeDots} alt='Icon Three Dots'/>
+        <img
+          className='p-2 rounded-full hover:bg-pastelgray'
+          src={iconThreeDots}
+          alt='Icon Three Dots'
+          onClick={() => showOptionsWindow()}
+        />
+        {/** Start of hidden menu */}
+        <PostItemOptions
+          classList={menuState ? ' ' : 'hidden'}
+          disabledState={disabledState}
+          setDisabledState={setDisabledState}
+        />
+        {/** End of hidden menu */}
       </header>
       <section>
-        <p className='pl-20 pr-6 pb-4'>{item.post}</p>
+        <form>
+          <textarea
+            id='postbody'
+            className={`w-10/12 ml-20 overflow-hidden resize-none ${disabledState ? 'bg-white' : 'bg-pastelgray'}`}
+            rows='4'
+            disabled={disabledState}
+            >
+              {item.post}
+          </textarea>
+          <span className='block ml-20 w-10/12 text-red-500 bg-red-100'>Error message</span>
+          <div className={`flex gap-2 justify-end mr-4 mb-3 ${disabledState ? 'hidden' : ' '}`}>
+            <button
+              className='py-1 px-3 text-white font-semibold rounded-full bg-pastelblue'
+              onClick={e => setDisabledState(true)}
+              >
+                Modificar
+            </button>
+            <button
+              className='py-1 px-3 text-white font-semibold rounded-full bg-pastelblue'
+              onClick={e => setDisabledState(true)}
+              >
+                Cancelar
+            </button>
+          </div>
+        </form>
         <img src={imgPostExample} alt='Example Image' />
       </section>
       <footer className='flex flex-wrap'>
         <button className='flex justify-center gap-2 basis-2/4 py-5'>
-          <img src={likeIcon} alt="Like" />
+          <img src={likeIcon} alt='Like' />
           <span>Like</span>
         </button>
-        <button className='flex justify-center gap-2 basis-2/4 py-5' onClick={() => toggleState()}>
-          <img src={commentIcon} alt="Comment" />
+        <button className='flex justify-center gap-2 basis-2/4 py-5' onClick={() => changeCommentState()}>
+          <img src={commentIcon} alt='Comment' />
           <span>Comment</span>
         </button>
         {/** Start of hidden comments */}
-        <div className={`basis-full ${state ? 'hola' : 'hidden'}`}>
+        <div className={`basis-full ${commentState ? ' ' : 'hidden'}`}>
           <form className='flex gap-3 basis-full p-4 border-t-2'>
-            <input className='grow px-4 bg-pastelgray rounded-3xl' type='text' placeholder='Agregar un comentario...' />
-            <input className='py-2 px-2 text-white font-semibold bg-pastelred rounded-full' type='image' src={messageIcon} onClick={e => sendComment(e)}/>
+            <textarea
+            name=''
+            rows='3'
+            placeholder='Agregar un comentario...'
+            className='w-full py-3 px-5 bg-pastelgray rounded-xl overflow-hidden resize-none'
+          >
+          </textarea>
+            <input className='self-start py-2 px-2 text-white font-semibold bg-pastelred rounded-full' type='image' src={messageIcon} onClick={e => sendComment(e)}/>
           </form>
           <ul>
             {
