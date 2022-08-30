df50e365187f9d5b634ef12aefddb8561e8a8e98
camiAguirre4rb1 <camila.aguirre@arbusta.net>
2022-08-30 12:00:02 -0300

    middlewares upPost - upAvatar

--- back/middlewares/upAvatar.js
+++ back/middlewares/upAvatar.js
@@ -4,8 +4,8 @@ const multer = require('multer')
 const path = require('path');
 const storage = multer.diskStorage({ //To save img
     destination: function (req, file, cb) {
-      cb(null,'public/users') //img destination from edit form
-    },/* path.join(__dirname,'..','..','front','src','img','users') */
+      cb(null,path.join(__dirname,'..','..','front','src','img','users')) //img destination from edit form
+    },/*  */
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)) 
